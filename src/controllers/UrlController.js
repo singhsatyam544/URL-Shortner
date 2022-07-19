11a010fe0eff744eab72bdbf6d9fba299ff4ad55
sandy9601 <darshanamsandeep375@gmail.com>
2022-07-19 10:56:45 +0530

    updated

--- src/controllers/UrlController.js
+++ src/controllers/UrlController.js
@@ -1,83 +1,74 @@
 const urlModel = require("../model/urlModel");
 const shortid = require("shortid");
-
-
-
-
-
+ 
 //*------------------------------------------------ShortnerUrlCreation---------------------------------------------------------------------------------
 
 const UrlShortner = async function (req, res) {
-    try{
-  const data = req.body;
-  const Urldata = {};
-  const baseUrl = "http://localhost:3000/";
-  Urldata.longUrl = data.longUrl;
-  if (
-    !/^(http(s)?:\/\/)?(www.)?([a-zA-Z0-9])+([\-\.]{1}[a-zA-Z0-9]+)*\.[a-zA-Z]{2,5}(:[0-9]{1,5})?(\/[^\s]*)?$/.test(
-      data.longUrl
-    )
-  ) {
+  try {
+    const data = req.body;
+    const Urldata = {};
+    const baseUrl = "http://localhost:3000";
+    Urldata.longUrl = data.longUrl
+    if(typeof data.longUrl=='string')
+    Urldata.longUrl=data.longUrl.trim()
     
-    return res
-      .status(400)
-      .send({ statua: false, message: "please enter a valid URL" });
-  }
+    if (
+      !/^(http(s)?:\/\/)?(www.)?([a-zA-Z0-9])+([\-\.]{1}[a-zA-Z0-9]+)*\.[a-zA-Z]{2,5}(:[0-9]{1,5})?(\/[^\s]*)?$/.test(
+        Urldata.longUrl
+      )
+    ) {
+      return res
+        .status(400)
+        .send({ statua: false, message: "please enter a valid URL" });
+    }
 
-  const checkLongUrl = await urlModel
-    .findOne({ longUrl: data.longUrl })
-    .select({ longUrl: 1, shortUrl: 1, urlCode: 1, _id: 0 });
-  if (checkLongUrl) {
-    return res
-      .status(400)
-      .send({
+    const checkLongUrl = await urlModel
+      .findOne({ longUrl: Urldata.longUrl })
+      .select({ longUrl: 1, shortUrl: 1, urlCode: 1, _id: 0 });
+    if (checkLongUrl) {
+      return res.status(400).send({
         status: false,
         message: "Url is already used",
         data: checkLongUrl,
       });
-  }
-  const urlgenerate = shortid.generate(data.longUrl);
-  const shortUrl = baseUrl + urlgenerate;
-  Urldata.urlCode = urlgenerate;
-  Urldata.shortUrl = shortUrl;
-  const urlshort = await urlModel.create(Urldata);
-  let resdata = {
-    longUrl: urlshort.longUrl,
-    shortUrl: urlshort.shortUrl,
-    urlCode: urlshort.urlCode,
-  };
-  res
-    .status(201)
-    .send({
+    }
+    const urlgenerate = shortid.generate(data.longUrl);
+    const shortUrl = baseUrl + "/"+urlgenerate;
+    Urldata.urlCode = urlgenerate;
+    Urldata.shortUrl = shortUrl;
+    const urlshort = await urlModel.create(Urldata);
+    let resdata = {
+      longUrl: urlshort.longUrl,
+      shortUrl: urlshort.shortUrl,
+      urlCode: urlshort.urlCode,
+    };
+    res.status(201).send({
       status: true,
       message: "url shortnered susscessfully ",
       data: resdata,
     });
-}
-catch(error){
-    res.status(500).send({status:false,error:error.message})
-}
-}
-
+  } catch (error) {
+    res.status(500).send({ status: false, error: error.message });
+  }
+};
 
 //*------------------------------------------------GetUrl---------------------------------------------------------------------------------
+
 const GetUrl = async function (req, res) {
-    try{
-  const paramCode = req.params.urlcode;
-  const longCheck = await urlModel.findOne({ urlCode: paramCode });
-  if (!longCheck) {
-    return res
-      .status(404)
-      .send({
+  try {
+    const paramCode = req.params.urlcode;
+    const longCheck = await urlModel.findOne({ urlCode: paramCode });
+    if (!longCheck) {
+      return res.status(404).send({
         status: false,
-        message: "Url is invalid"
-              });
+        message: "Url is invalid",
+      });
+    }
+    const resLogin = longCheck.longUrl;
+    res.status(302).redirect(resLogin); // TA question
+  } catch (error) {
+    res.status(500).send({ status: false, error: error.message });
   }
-  const resLogin = longCheck.longUrl;
-  res.status(302).redirect(resLogin);
-}
-catch(error){
-    res.status(500).send({status:false,error:error.message})
-}}
+};
 
 module.exports = { UrlShortner, GetUrl };
