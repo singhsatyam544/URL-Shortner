0466514db97ec68973232798ef77b8d4cc1d7e19
sandy9601 <darshanamsandeep375@gmail.com>
2022-07-20 17:14:39 +0530

    redis in create Url

--- src/controllers/UrlController.js
+++ src/controllers/UrlController.js
@@ -18,8 +18,6 @@ redisClient.on("connect", async function () {
   console.log("Connected to Redis..");
 });
 
-
-
 //1. connect to the server
 //2. use the commands :
 
@@ -30,15 +28,15 @@ const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
 
 //*------------------------------------------------ShortnerUrlCreation---------------------------------------------------------------------------------
 
+
 const UrlShortner = async function (req, res) {
   try {
     const data = req.body;
     const Urldata = {};
     const baseUrl = "http://localhost:3000";
-    Urldata.longUrl = data.longUrl
-    if(typeof data.longUrl=='string')
-    Urldata.longUrl=data.longUrl.trim()
-    
+    Urldata.longUrl = data.longUrl;
+    if (typeof data.longUrl == "string") Urldata.longUrl = data.longUrl.trim();
+
     if (
       !/^(http(s)?:\/\/)?(www.)?([a-zA-Z0-9])+([\-\.]{1}[a-zA-Z0-9]+)*\.[a-zA-Z]{2,5}(:[0-9]{1,5})?(\/[^\s]*)?$/.test(
         Urldata.longUrl
@@ -49,27 +47,40 @@ const UrlShortner = async function (req, res) {
         .send({ statua: false, message: "please enter a valid URL" });
     }
 
-    const checkLongUrl = await urlModel
-      .findOne({ longUrl: Urldata.longUrl })
-      .select({ longUrl: 1, shortUrl: 1, urlCode: 1, _id: 0 });
-    if (checkLongUrl) {
-      return res.status(400).send({
-        status: false,
-        message: "Url is already used",
-        data: checkLongUrl,
-      });
+    let cahcedProfileData = await GET_ASYNC(`${Urldata.longUrl}`);
+    if (cahcedProfileData) {
+      return res
+        .status(400)
+        .send({
+          status: false,
+          message: "url is already used",
+          data: cahcedProfileData,
+        });
+    } else {
+      const checkLongUrl = await urlModel
+        .findOne({ longUrl: Urldata.longUrl })
+        .select({ longUrl: 1, shortUrl: 1, urlCode: 1, _id: 0 });
+      if (checkLongUrl) {
+        return res.status(400).send({
+          status: false,
+          message: "Url is already used",
+          data: checkLongUrl,
+        });
+      }
     }
     const urlgenerate = shortid.generate();
-    const shortUrl = baseUrl + "/"+urlgenerate;
+    const shortUrl = baseUrl + "/" + urlgenerate;
     Urldata.urlCode = urlgenerate;
     Urldata.shortUrl = shortUrl;
     const urlshort = await urlModel.create(Urldata);
+
     let resdata = {
       longUrl: urlshort.longUrl,
       shortUrl: urlshort.shortUrl,
       urlCode: urlshort.urlCode,
     };
-    res.status(201).send({
+    await SET_ASYNC(`${Urldata.longUrl}`, JSON.stringify(resdata));
+    return res.status(201).send({
       status: true,
       message: "url shortnered susscessfully ",
       data: resdata,
@@ -89,19 +100,18 @@ const UrlShortner = async function (req, res) {
 //       await SET_ASYNC(`${req.params.authorId}`, JSON.stringify(profile))
 //       res.send({ data: profile });
 //     }
-  
+
 //   };
 
 const GetUrl = async function (req, res) {
-    try {
-      const paramCode = req.params.urlcode;
-      let cahcedProfileData = await GET_ASYNC(`${paramCode}`)
-     if(cahcedProfileData) {
-       res.send(cahcedProfileData)
-    }
-else{
+  try {
+    const paramCode = req.params.urlcode;
+    let cahcedProfileData = await GET_ASYNC(`${paramCode}`);
+    if (cahcedProfileData) {
+      return res.status(302).send(cahcedProfileData);
+    } else {
       const longCheck = await urlModel.findOne({ urlCode: paramCode });
-     await SET_ASYNC(`${paramCode}`, JSON.stringify(longCheck))
+      await SET_ASYNC(`${paramCode}`, JSON.stringify(longCheck));
 
       if (!longCheck) {
         return res.status(404).send({
@@ -110,12 +120,11 @@ else{
         });
       }
       const resLogin = longCheck.longUrl;
-      res.status(302).redirect(resLogin); // TA question
-    }} catch (error) {
-      res.status(500).send({ status: false, error: error.message });
+      return res.status(302).redirect(resLogin); // TA question
     }
+  } catch (error) {
+    res.status(500).send({ status: false, error: error.message });
   }
-  
-
+};
 
 module.exports = { UrlShortner, GetUrl };
