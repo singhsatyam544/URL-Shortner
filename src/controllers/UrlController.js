22ac2ece6d0b6fd230c2cfceaadaa1929cf0e987
sandy9601 <darshanamsandeep375@gmail.com>
2022-07-20 16:26:16 +0530

    redis

--- src/controllers/UrlController.js
+++ src/controllers/UrlController.js
@@ -1,6 +1,33 @@
 const urlModel = require("../model/urlModel");
 const shortid = require("shortid");
- 
+const redis = require("redis");
+
+const { promisify } = require("util");
+
+//Connect to redis
+const redisClient = redis.createClient(
+  13190,
+  "redis-13190.c301.ap-south-1-1.ec2.cloud.redislabs.com",
+  { no_ready_check: true }
+);
+redisClient.auth("gkiOIPkytPI3ADi14jHMSWkZEo2J5TDG", function (err) {
+  if (err) throw err;
+});
+
+redisClient.on("connect", async function () {
+  console.log("Connected to Redis..");
+});
+
+
+
+//1. connect to the server
+//2. use the commands :
+
+//Connection setup for redis
+
+const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
+const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
+
 //*------------------------------------------------ShortnerUrlCreation---------------------------------------------------------------------------------
 
 const UrlShortner = async function (req, res) {
@@ -32,7 +59,7 @@ const UrlShortner = async function (req, res) {
         data: checkLongUrl,
       });
     }
-    const urlgenerate = shortid.generate(data.longUrl);
+    const urlgenerate = shortid.generate();
     const shortUrl = baseUrl + "/"+urlgenerate;
     Urldata.urlCode = urlgenerate;
     Urldata.shortUrl = shortUrl;
@@ -53,22 +80,42 @@ const UrlShortner = async function (req, res) {
 };
 
 //*------------------------------------------------GetUrl---------------------------------------------------------------------------------
+//const fetchAuthorProfile = async function (req, res) {
+//     let cahcedProfileData = await GET_ASYNC(`${req.params.authorId}`)
+//     if(cahcedProfileData) {
+//       res.send(cahcedProfileData)
+//     } else {
+//       let profile = await authorModel.findById(req.params.authorId);
+//       await SET_ASYNC(`${req.params.authorId}`, JSON.stringify(profile))
+//       res.send({ data: profile });
+//     }
+  
+//   };
 
 const GetUrl = async function (req, res) {
-  try {
-    const paramCode = req.params.urlcode;
-    const longCheck = await urlModel.findOne({ urlCode: paramCode });
-    if (!longCheck) {
-      return res.status(404).send({
-        status: false,
-        message: "Url is invalid",
-      });
+    try {
+      const paramCode = req.params.urlcode;
+      let cahcedProfileData = await GET_ASYNC(`${paramCode}`)
+     if(cahcedProfileData) {
+       res.send(cahcedProfileData)
     }
-    const resLogin = longCheck.longUrl;
-    res.status(302).redirect(resLogin); // TA question
-  } catch (error) {
-    res.status(500).send({ status: false, error: error.message });
+else{
+      const longCheck = await urlModel.findOne({ urlCode: paramCode });
+     await SET_ASYNC(`${paramCode}`, JSON.stringify(longCheck))
+
+      if (!longCheck) {
+        return res.status(404).send({
+          status: false,
+          message: "Url is invalid",
+        });
+      }
+      const resLogin = longCheck.longUrl;
+      res.status(302).redirect(resLogin); // TA question
+    }} catch (error) {
+      res.status(500).send({ status: false, error: error.message });
+    }
   }
-};
+  
+
 
 module.exports = { UrlShortner, GetUrl };
